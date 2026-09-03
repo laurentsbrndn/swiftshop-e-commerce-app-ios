21ad84c8255deaceb396ef8dd04f194b47aa6ac1
laurentsbrndn <brandonvikario607@gmail.com>
2026-09-03 16:07:17 +0700

    Initial Commit

---
+++ simple-e-commerce-app-ios/simple_e_commerce_app_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  simple_e_commerce_app_iosApp.swift
+//  simple-e-commerce-app-ios
+//
+//  Created by Laurentius Brandon Vikario on 03/09/26.
+//
+
+import SwiftUI
+
+@main
+struct simple_e_commerce_app_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
